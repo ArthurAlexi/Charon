b5a3402d973de51b94417119f2f2e6390e9ceeec
Arthur Alexi <arthuralexi@hotmail.com>
2024-04-10 06:41:08 -0300

    fix: imports in register-for-events

--- src/routes/register-for-event.ts
+++ src/routes/register-for-event.ts
@@ -1,4 +1,4 @@
-import { prisma } from './../libs/prisma';
+import { prisma } from '../libs/prisma';
 import { FastifyInstance } from "fastify";
 import { z } from 'zod'
 import { ZodTypeProvider } from "fastify-type-provider-zod";
