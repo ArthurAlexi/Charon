cf798e726b589d31bef02380644db3bf3717b83c
Arthur Alexi <arthuralexi@hotmail.com>
2024-04-13 21:18:24 -0300

    feat: create route get to take attendees by EventId

--- src/server.ts
+++ src/server.ts
@@ -4,6 +4,7 @@ import { getEvent } from "./routes/get-event"
 import { registerForEvent } from "./routes/register-for-event"
 import { getAttendeeBadge } from "./routes/get-attendee-badge"
 import { checkIn } from "./routes/check-in"
+import { getEventAttendees } from "./routes/get-event-attendees"
 
 const app = fastify()
 
@@ -16,6 +17,7 @@ app.register(getEvent)
 app.register(registerForEvent)
 app.register(getAttendeeBadge)
 app.register(checkIn)
+app.register(getEventAttendees)
 
 app.listen({
     port: 3333
