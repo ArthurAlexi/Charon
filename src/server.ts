1134178191fa89c6d485b2e29a2eaa96a040a3d7
Arthur Alexi <arthuralexi@hotmail.com>
2024-04-15 08:51:55 -0300

    feat: add Error Handler

--- src/server.ts
+++ src/server.ts
@@ -9,9 +9,12 @@ import { registerForEvent } from "./routes/register-for-event"
 import { getAttendeeBadge } from "./routes/get-attendee-badge"
 import { checkIn } from "./routes/check-in"
 import { getEventAttendees } from "./routes/get-event-attendees"
+import { errorHandler } from "./error-handler"
 
 
 const app = fastify()
+
+/** SETTINGS */
 
 app.setValidatorCompiler(validatorCompiler)
 app.setSerializerCompiler(serializerCompiler)
@@ -28,12 +31,17 @@ app.register(fastifySwagger, {
     transform: jsonSchemaTransform
 })
 
-app.register(fastifySwaggerUi, {
-    routePrefix: '/docs',
-})
+app.setErrorHandler(errorHandler)
 
+
+
+/** ROUTES */
 app.get('/', ()=> {
     return 'Hello World.'
+})
+
+app.register(fastifySwaggerUi, {
+    routePrefix: '/docs',
 })
 
 app.register(createEvent)
@@ -42,6 +50,8 @@ app.register(registerForEvent)
 app.register(getAttendeeBadge)
 app.register(checkIn)
 app.register(getEventAttendees)
+
+
 
 app.listen({
     port: 3333
