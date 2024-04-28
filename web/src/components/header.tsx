793b6defb123524aba9335866726b20eaea2a583
Arthur Alexi <arthuralexi@hotmail.com>
2024-04-28 16:14:36 -0300

    feat: create a nav link component

--- web/src/components/header.tsx
+++ web/src/components/header.tsx
@@ -1,12 +1,13 @@
 import logo from '../../public/logo.png'
+import { NavLink } from './nav-link'
 
 export function Header(){
     return(
         <header className='flex items-center gap-5 py-2'>
             <img src={logo} alt='charon-logo' className='rounded-md border border-solid border-black' />
             <nav className='flex items-center gap-5'>
-                <a href="#" className='font-medium text-sm text-zinc-300'>Events</a>
-                <a href="#" className='font-medium text-sm'>Attendees</a>
+                <NavLink>Events</NavLink>
+                <NavLink>Attendees</NavLink>
             </nav>
         </header>
     )
