0971c0ac21d2eca7ec283938ef9d4509beb21644
Arthur Alexi <arthuralexi@hotmail.com>
2024-05-30 14:53:51 -0300

    feat: add google fonts roboto

--- mobile/src/app/_layout.tsx
+++ mobile/src/app/_layout.tsx
@@ -1,6 +1,22 @@
 import {Slot} from "expo-router";
+import { StatusBar } from "expo-status-bar";
+import { useFonts, Roboto_700Bold, Roboto_500Medium, Roboto_400Regular } from "@expo-google-fonts/roboto";
+import { Loading } from "../components/Loading";
 import '../styles/global.css'
 
 export default function Layout(){
-    return <Slot />
+    const [fontsLoaded] = useFonts({
+        Roboto_700Bold, Roboto_500Medium, Roboto_400Regular
+    })
+
+    if(! fontsLoaded) {
+        return
+    }
+
+    return (
+        <>  
+            <StatusBar style="light"/>
+            { fontsLoaded ? <Slot /> : <Loading /> }
+        </>
+    )
 }
