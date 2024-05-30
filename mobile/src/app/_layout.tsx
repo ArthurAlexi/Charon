b1b2177ac385f722159eca9a9b552f5ee63bf47a
Arthur Alexi <arthuralexi@hotmail.com>
2024-05-30 15:30:19 -0300

    feat: create Input Component

--- mobile/src/app/_layout.tsx
+++ mobile/src/app/_layout.tsx
@@ -1,8 +1,10 @@
+import '@/styles/global.css'
+
 import {Slot} from "expo-router";
 import { StatusBar } from "expo-status-bar";
 import { useFonts, Roboto_700Bold, Roboto_500Medium, Roboto_400Regular } from "@expo-google-fonts/roboto";
-import { Loading } from "../components/Loading";
-import '../styles/global.css'
+import { Loading } from "@/components/Loading";
+
 
 export default function Layout(){
     const [fontsLoaded] = useFonts({
--- mobile/src/app/index.tsx
+++ mobile/src/app/index.tsx
@@ -1,9 +1,24 @@
-import {View, Text} from 'react-native'
+import { Input } from '@/components/input'
+import {View, Image} from 'react-native'
+import { MaterialCommunityIcons } from "@expo/vector-icons"
+import { colors } from '@/styles/colors'
 
 export default function Home(){
     return (
-        <View>
-            <Text> Hellow World! </Text>
+        <View className="flex-1 bg-green-500 justify-center items-center">
+            <Image source={require("@/assets/logo.png")} 
+                className='h-16'  
+                resizeMode='contain'
+            />
+            <view className='w-full mt-12 gap-3'>
+                <Input>
+                    <MaterialCommunityIcons name='ticket-confirmation-outline'
+                        size={20}
+                        color={colors.green[200]}
+                    />
+                    <Input.Field placeholder='ticket code...'/>
+                </Input>
+            </view>
         </View>
     )
 }
