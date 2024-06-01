7b0a1a845a5866d5b66627e1be31354447f5a49d
Arthur Alexi <arthuralexi@hotmail.com>
2024-06-01 16:15:14 -0300

    feat: start form actions e validations

--- mobile/src/app/index.tsx
+++ mobile/src/app/index.tsx
@@ -1,33 +1,49 @@
 import { Input } from '@/components/input'
-import { View, Image, StatusBar } from 'react-native'
+import { View, Image, StatusBar, Alert } from 'react-native'
 import { MaterialCommunityIcons } from "@expo/vector-icons"
 import { colors } from '@/styles/colors'
 import { Button } from '@/components/button'
-import { Link } from 'expo-router'
+import { Link, Redirect, router } from 'expo-router'
+import { useState } from 'react'
 
 export default function Home() {
+
+    const [code, setCode] = useState("")
+
+    function handleAcessCredential() {
+        // console.warn(code)
+        if (!code.trim()) {
+            return Alert.alert("Ticket", "Code is empity.")
+        }
+
+        router.push("/ticket")
+
+    }
+
     return (
         <View className="flex-1 bg-green-500 justify-center items-center">
 
-            <StatusBar barStyle="light-content"  />
+            <StatusBar barStyle="light-content" />
 
             <Image source={require("@/assets/logo.png")}
                 className='h-16'
                 resizeMode='contain'
             />
-            <view className='w-full mt-12 gap-3'>
+            <View className='w-full mt-12 gap-3 px-10'>
                 <Input>
                     <MaterialCommunityIcons name='ticket-confirmation-outline'
                         size={20}
                         color={colors.green[200]}
                     />
-                    <Input.Field placeholder='ticket code...' />
+                    <Input.Field
+                        onChangeText={setCode}
+                        placeholder='ticket code...' />
                 </Input>
-                <Button title='acess credentials' />
-                <Link href="/#" className="text-gray-100 text-base font-bold text-center mt-8" >
+                <Button title='acess credentials' onPress={handleAcessCredential} />
+                <Link href="/register" className="text-gray-100 text-base font-bold text-center mt-8" >
                     Don't have a ticket yet?
                 </Link>
-            </view>
+            </View>
         </View>
     )
 }
--- mobile/src/app/register.tsx
+++ mobile/src/app/register.tsx
@@ -1,40 +1,52 @@
 import { Input } from '@/components/input'
-import { View, Image, StatusBar } from 'react-native'
+import { View, Image, StatusBar, Alert } from 'react-native'
 import { FontAwesome6, MaterialIcons } from "@expo/vector-icons"
 import { colors } from '@/styles/colors'
-import { Button } from '@/components/button'
+
 import { Link } from 'expo-router'
+import { useState } from 'react'
+import { Button } from '@/components/button'
 
 export default function Register() {
+
+    const [userName, setUserName] = useState("")
+    const [email, setEmail] = useState("")
+
+    function handleRegister() {
+        if (!userName.trim() || !email.trim) {
+            return Alert.alert("Register", "Enter all fields.")
+        }
+    }
+
     return (
         <View className="flex-1 bg-green-500 justify-center items-center">
 
-            <StatusBar barStyle="light-content"  />
+            <StatusBar barStyle="light-content" />
 
             <Image source={require("@/assets/logo.png")}
                 className='h-16'
                 resizeMode='contain'
             />
-            <view className='w-full mt-12 gap-3'>
+            <View className='w-full mt-12 gap-3 '>
                 <Input>
                     <FontAwesome6 name='user-circle'
                         size={20}
                         color={colors.green[200]}
                     />
-                    <Input.Field placeholder='Complete name...' />
+                    <Input.Field placeholder='Complete name...' onChangeText={setUserName} />
                 </Input>
                 <Input>
                     <MaterialIcons name='alternate-email'
                         size={20}
                         color={colors.green[200]}
                     />
-                    <Input.Field placeholder='e-mail...'  keyboardType='email-address'/>
+                    <Input.Field placeholder='e-mail...' keyboardType='email-address' onChangeText={setEmail} />
                 </Input>
-                <Button title='Register' />
+                <Button title='Register' onPress={handleRegister} />
                 <Link href="/" className="text-gray-100 text-base font-bold text-center mt-8">
                     Already have a ticket?
                 </Link>
-            </view>
+            </View>
         </View>
     )
 }
