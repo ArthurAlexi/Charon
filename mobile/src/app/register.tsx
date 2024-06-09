f46e8c6201a27961ef2055cc3293729195f6648e
Arthur Alexi <arthuralexi@hotmail.com>
2024-06-09 14:25:45 -0300

    feat: connect register action with API

--- mobile/src/app/register.tsx
+++ mobile/src/app/register.tsx
@@ -3,19 +3,51 @@ import { View, Image, StatusBar, Alert } from 'react-native'
 import { FontAwesome6, MaterialIcons } from "@expo/vector-icons"
 import { colors } from '@/styles/colors'
 
-import { Link } from 'expo-router'
+import { Link, router } from 'expo-router'
 import { useState } from 'react'
 import { Button } from '@/components/button'
+import { api } from '@/libs/api'
+import axios from 'axios'
 
 export default function Register() {
 
     const [userName, setUserName] = useState("")
     const [email, setEmail] = useState("")
+    const [isLoading, setIsLoading] = useState(false)
 
-    function handleRegister() {
-        if (!userName.trim() || !email.trim) {
-            return Alert.alert("Register", "Enter all fields.")
+    async function handleRegister() {
+        try{
+            if (!userName.trim() || !email.trim) {
+                return Alert.alert("Register", "Enter all fields.")
+            }
+            setIsLoading(true)
+            const eventId = ""
+            const result = await api.post(`/events/${eventId}/attendees`, {
+                name: userName.trim(),
+                email: email.trim(),
+            })
+
+            if(result.data.attendeeId){
+                Alert.alert("register", "registration completed successfully",[
+                    {text: "ok", onPress: ()=> router.push("/ticket")}
+                ])
+            }
+            
+        }catch(error){
+            console.error(error)
+            var errorMessage = "Unable to register"
+
+            if(axios.isAxiosError(error)){
+                if(String(error.response?.data?.message).includes("already registed")){
+                    errorMessage = error.response?.data?.message
+                }
+            }
+
+            Alert.alert("Register", "Unable to register")  
+        }finally{
+            setIsLoading(false)
         }
+
     }
 
     return (
@@ -42,7 +74,7 @@ export default function Register() {
                     />
                     <Input.Field placeholder='e-mail...' keyboardType='email-address' onChangeText={setEmail} />
                 </Input>
-                <Button title='Register' onPress={handleRegister} />
+                <Button title='Register' onPress={handleRegister} isLoading={isLoading} />
                 <Link href="/" className="text-gray-100 text-base font-bold text-center mt-8">
                     Already have a ticket?
                 </Link>
